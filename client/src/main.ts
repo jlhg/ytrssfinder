09243dcab1ed596e94add251b3696f9b68746111
Jian-Long Huang <contact@jlhg.org>
2023-11-07 14:32:39 +0800

    client: Change a color to inherit

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import { Quasar } from 'quasar'
 import 'quasar/src/css/index.sass'
 import '@quasar/extras/fontawesome-v6/fontawesome-v6.css'
-// import './style.scss'
+import './style.scss'
 import App from './App.vue'
 
 const app = createApp(App)
